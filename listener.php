8b2c9018e464e730e4a7830075080529078b23d1
Hamza Betouar <betouar.hamza.89@gmail.com>
2018-09-18 12:49:39 +0100

    create listener for user

--- listener.php
+++ listener.php
@@ -10,6 +10,16 @@ $connection = new Connection('tcp://localhost:61613');
 $stomp = new StatefulStomp(new Client($connection));
 
 if( !isset($argv[1]) ) {
-    echo "error syntax : php sender.php user";
+    echo "error syntax : php listener.php user";
     die;
+}
+
+$stomp->subscribe('/tchat-u'.$argv[1]);
+while(true){
+    echo "In listening for user {$argv[1]}\n";
+    /** Frame $message */
+    while($message = $stomp->read()){
+        $response = json_decode($message->getBody());
+        echo "Message from {$response->sender} : '{$response->message}'\n";
+    }
 }
